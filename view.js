ead7c0d7b45f318ee9c736aaabf0ac7581006f6c
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-10-18 13:31:48 +0200

    Update view.js

---
+++ view.js
@@ -0,0 +1,63 @@
+updateView()
+function updateView(){
+model.app.div.innerHTML = /*html*/`
+
+<h1>Daniels Bilverksted</h1>
+<div>${showAddCars()}</div>
+<div>${showCars()}</div>
+
+
+
+`;
+}
+function showCars(){
+let html = "";
+for(let i = 0; i < model.data.cars.length; i++){
+    html += /*html*/`
+    
+    <div>
+    <h3>brand: ${model.data.cars[i].brand}</h3>
+    <p>year: ${model.data.cars[i].year}</p>
+    <p>color: ${model.data.cars[i].color}</p>
+    <p>licensePlate: ${model.data.cars[i].licensePlate}</p>
+    <p>kmhDistance: ${model.data.cars[i].kmhDistance}</p>
+    </div>
+    
+    
+    
+    `;
+}
+return html;
+
+}
+
+function showAddCars(){
+    let html = "";
+    html += /*html*/`
+        <h3>Add car</h3>
+        <div>
+            <label>brand:</label>
+            <input type="text">
+        </div>
+        <div>
+            <label>year:</label>
+            <input type="number">
+        </div>
+        <div>
+            <label>color:</label>
+            <input type="text">
+        </div>
+        <div>
+            <label>licensePlate:</label>
+            <input type="text">
+        </div>
+        <div>
+            <label>kmhDistance:</label>
+            <input type="number">
+        </div>
+
+    `;
+    return html;
+
+
+}
