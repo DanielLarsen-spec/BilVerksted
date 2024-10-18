4a8c16d2974baa9baa3643c798560849781070ca
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-10-18 12:59:47 +0200

    Update model.js

---
+++ model.js
@@ -0,0 +1,41 @@
+const model = {
+    app:{
+        div: document.getElementById('app')
+
+    },
+
+
+    input:{
+        newCar:{
+            brand: null,
+            year: null,
+            color: null,
+            licensePlate: null,
+            kmhDistance: null,
+        }
+
+        },
+
+     data:{
+        cars:[
+            {
+                brand: 'ford',
+                year: 1997,
+                color: 'brown',
+                licensePlate: 'LS25678',
+                kmhDistance: 254999,
+            },
+            {
+                brand: 'Volvo',
+                year: 2006,
+                color: 'black',
+                licensePlate: 'LJ39838',
+                kmhDistance: 148777,
+            },
+        ]
+
+
+
+
+     }   
+}
